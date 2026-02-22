86974e03792d88668b88d29fad1db499cd565284
Nataole <Nataole2025@yandex.ru>
2026-02-23 01:50:24 +0300

    Fix Task

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -2,77 +2,78 @@ package tasks;
 
 import java.util.Objects;
 
-    public class Task {
+public class Task {
 
-        protected int id;
-        protected String title;
-        protected String description;
-        protected Status status;
+    protected int id;
+    protected String title;
+    protected String description;
+    protected Status status;
 
-        public Task(String title, String description, Status status) {
-            this.title = title;
-            this.description = description;
-            this.status = status;
-        }
-        public Task copy() {
-            Task copy = new Task(this.title, this.description, this.status);
-            copy.setId(this.id);
-            return copy;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public Status getStatus() {
-            return status;
-        }
-
-        public void setStatus(Status status) {
-            this.status = status;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Task task = (Task) o;
-            return id == task.id;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id);
-        }
-
-        @Override
-        public String toString() {
-            return "Task{" +
-                    "id=" + id +
-                    ", title='" + title + '\'' +
-                    ", description='" + description + '\'' +
-                    ", status=" + status +
-                    '}';
-        }
+    public Task(String title, String description, Status status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
     }
+
+    public Task copy() {
+        Task copy = new Task(this.title, this.description, this.status);
+        copy.setId(this.id);
+        return copy;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
 
