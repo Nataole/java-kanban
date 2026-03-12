cb5a52d7c04f669d230b865c7b5e9acaf7c4ef30
Nataole <Nataole2025@yandex.ru>
2026-03-12 20:54:23 +0400

    Fix checkstyle

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -3,52 +3,53 @@ package tasks;
 import java.util.ArrayList;
 import java.util.List;
 
-    public class Epic extends Task {
-        private final List<Integer> subtaskIds;
+public class Epic extends Task {
+    private final List<Integer> subtaskIds;
 
-        public Epic(String title, String description) {
-            super(title, description, Status.NEW);
-            this.subtaskIds = new ArrayList<>();
-        }
-
-        public List<Integer> getSubtaskIds() {
-            return new ArrayList<>(subtaskIds);
-        }
-
-        public void addSubtaskId(int subtaskId) {
-            subtaskIds.add(subtaskId);
-        }
-
-        public void removeSubtaskId(int subtaskId) {
-            subtaskIds.remove((Integer) subtaskId);
-        }
-
-        public void clearSubtaskIds() {
-            subtaskIds.clear();
-        }
-
-        @Override
-        public String toString() {
-            return "Epic{" +
-                    "id=" + id +
-                    ", title='" + title + '\'' +
-                    ", description='" + description + '\'' +
-                    ", status=" + status +
-                    ", subtaskIds=" + subtaskIds +
-                    '}';
-        }
-        @Override
-        public Epic copy() {
-            Epic copy = new Epic(this.title, this.description);
-            copy.setId(this.id);
-            copy.setStatus(this.status);
-
-            // сохраняем список подзадач (чтобы копия эпика была корректной)
-            for (Integer subId : this.getSubtaskIds()) {
-                copy.addSubtaskId(subId);
-            }
-            return copy;
-        }
-
+    public Epic(String title, String description) {
+        super(title, description, Status.NEW);
+        this.subtaskIds = new ArrayList<>();
     }
+
+    public List<Integer> getSubtaskIds() {
+        return new ArrayList<>(subtaskIds);
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove((Integer) subtaskId);
+    }
+
+    public void clearSubtaskIds() {
+        subtaskIds.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+
+    @Override
+    public Epic copy() {
+        Epic copy = new Epic(this.title, this.description);
+        copy.setId(this.id);
+        copy.setStatus(this.status);
+
+        // сохраняем список подзадач (чтобы копия эпика была корректной)
+        for (Integer subId : this.getSubtaskIds()) {
+            copy.addSubtaskId(subId);
+        }
+        return copy;
+    }
+
+}
 
--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -1,29 +1,28 @@
 package tasks;
 
 public class Subtask extends Task {
-        private final int epicId;
+    private final int epicId;
 
-        public Subtask(String title, String description, Status status, int epicId) {
-            super(title, description, status);
-            this.epicId = epicId;
-        }
+    public Subtask(String title, String description, Status status, int epicId) {
+        super(title, description, status);
+        this.epicId = epicId;
+    }
 
-        public int getEpicId() {
-            return epicId;
-        }
+    public int getEpicId() {
+        return epicId;
+    }
 
 
-
-        @Override
-        public String toString() {
-            return "Subtask{" +
-                    "id=" + id +
-                    ", title='" + title + '\'' +
-                    ", description='" + description + '\'' +
-                    ", status=" + status +
-                    ", epicId=" + epicId +
-                    '}';
-        }
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", epicId=" + epicId +
+                '}';
+    }
 
     @Override
     public Subtask copy() {
